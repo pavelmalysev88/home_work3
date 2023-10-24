48a3b941ed9593f750e1b44bfc627f5c83bb0a88
Pavel Malyshev <pavelmalysev463@gmail.com>
2023-10-24 20:45:21 +0300

    123

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество элементов массива, 
+// значения которых лежат в отрезке [20,90].
+// массив [10 21 14 93 23] => 2
+
+
+int N=10;
+int[] a=new int[N];
+Random random=new Random();
+for(int i=0;i<a.Length;i++)
+a[i]=random.Next(1,100);
+for(int i=0;i<a.Length;i++)
+System.Console.Write($"{a[i],4}");
+
+int count=0;
+for(var i=0; i<a.Length;i++)
+{
+if(i>20 && i<90)
+count++;
+}
+System.Console.WriteLine();
+System.Console.WriteLine(count);
---
+++ Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте массив на 10 целых чисел. Напишите программу, 
+// которая определяет количество чётных чисел в массиве.
+// массив [6 7 19 34 3 1 4 7 9 1] => 3
+// массив [1 8 43 4 55 60 3 2 1 3] => 4
+
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Вот наш массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int z = 0; z < numbers.Length; z++)
+if (numbers[z] % 2 == 0)
+count++;
+
+Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(10,100);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
